a44dc8e63b97172940ed9408cfe433c1077f2a7d
Joseph Spirito <44129684+joseph-spirito@users.noreply.github.com>
2024-12-09 20:33:32 -0500

    Add files via upload

--- PCEP/Module 3/PCEP_decisions.py
+++ PCEP/Module 3/PCEP_decisions.py
@@ -72,3 +72,47 @@ print(n >= 100)
 ## else:
 ##     play_chess_at_home()
 
+## Challenge
+## Citizens making more than 85,258 "thalers", they are taxed 32% the surplus over the threshold PLUS 14,839.02 flat
+## Citizens making the threshold or less are taxed 18% minus 556.02
+
+## Given code
+income = float(input("Enter the annual income: "))
+
+## Write code here
+tax = 0.0
+if income <= 85258.0:
+    tax = (income * 0.18) - 556.02
+else:
+    tax = ((income - 85258.0) * 0.32) + 14839.02
+
+## Given code
+tax = round (tax, 0)
+print("The tax is:", tax, "thallers")
+
+## Challenge
+## Leap year identifier
+## if the year number isn't divisible by four, it's a common year
+## otherwise, if the year number isn't divisible by 100, it's a leap year
+## otherwise, if the year number isn't divisible by 400, it's a common year
+## otherwise, it's a leap year
+## NOTE Leap years did not start until 1582
+
+## Given code
+year = int(input("Enter a year: "))
+
+## Write code here
+type_of_year = ""
+
+if year < 1582:
+    type_of_year = "Not within the gregorian calendar period"
+elif (year % 4) != 0:
+    type_of_year = "Common year"
+elif (year % 100) != 0:
+    type_of_year = "Leap year"
+elif (year % 400) != 0:
+    type_of_year = "Common year"
+else:
+    type_of_year = "Leap year"
+
+print(type_of_year)
